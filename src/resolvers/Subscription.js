eb69a8895e79614e566fbbdb7015e2de338e2cc5
Gaurav Pandey <gaurav071@gmail.com>
2021-03-21 16:05:02 +0530

    GraphQL Subscriptions

---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,20 @@
+const newLinkSubscribe = (parent, args, context, info) =>
+  context.pubsub.asyncIterator("NEW_LINK");
+
+const newLink = {
+  subscribe: newLinkSubscribe,
+  resolve: (payload) => payload,
+};
+
+const newVoteSubscribe = (parent, args, context, info) =>
+  context.pubsub.asyncIterator("NEW_VOTE");
+
+const newVote = {
+  subscribe: newVoteSubscribe,
+  resolve: (payload) => payload,
+};
+
+module.exports = {
+  newLink,
+  newVote,
+};
